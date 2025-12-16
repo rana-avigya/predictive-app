06d2cf4ba5405251255591a635cf4e0263b7881b
Avigya Rana <avigyar@gmail.com>
2025-12-16 21:27:02 +0545

    added eda to app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import joblib
 from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title(" Predictive Model App")
 
@@ -23,6 +25,41 @@ st.sidebar.write(f"Problem type: {PROBLEM}")
 df = pd.read_csv("data.csv")
 DROP_COLS = ["customer_id", TARGET_COL] 
 input_cols = df.drop(columns=DROP_COLS, errors='ignore').columns
+
+st.header("Exploratory Data Analysis")
+
+st.subheader("Dataset Preview")
+st.dataframe(df.head())
+
+st.subheader("Column datatypes")
+st.write(df.dtypes)
+
+st.subheader("Summary")
+st.write(df.describe())
+
+st.subheader("Feature distribution")
+select_col = st.selectbox("Select column" ,df.columns)
+fig, ax = plt.subplots()
+
+if df[select_col].dtype in ["int64","float64"]:
+    sns.histplot(df[select_col], kde=True, ax=ax)
+    ax.set_ylabel(select_col)
+    ax.set_xlabel("Count")
+    ax.set_title(f"Distribution of {select_col}")
+else:
+    sns.countplot(df[select_col])
+    ax.set_ylabel(select_col)
+    ax.set_xlabel("Count")
+    ax.set_title(f"Distribution of {select_col}")
+st.pyplot(fig)
+
+numeric_cols = df.select_dtypes(include=["int64", "float64"])
+
+if len(numeric_cols.columns) > 1:
+    st.subheader("Correlation Heatmap")
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.heatmap(numeric_cols.corr(), annot=True, cmap="coolwarm", ax=ax)
+    st.pyplot(fig)
 
 st.header("Input Prediction")
 input = {}
